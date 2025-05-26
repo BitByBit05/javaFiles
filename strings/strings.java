14e54e239445691b379226c44e3fc5d9f5eeb5cf
kumarjitDas <kumarjitdas.coding@gmail.com>
2025-05-26 15:24:15 +0530

    strings

---
+++ strings/strings.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class strings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String username, password;
+        System.out.println("Enter username : ");
+        username = scanner.nextLine();
+        System.out.println("Enter password :");
+        password = scanner.nextLine();
+        System.out.println("Username: "+username+"\tPassword: "+password);
+        scanner.close();
+    }
+}

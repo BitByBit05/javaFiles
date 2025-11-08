46683740a9660c2cb21705d42e9ed93365978ce9
kumarjitDas <kumarjitdas.coding@gmail.com>
2025-11-08 10:07:02 +0530

    8th November

---
+++ ArrayList/intro.java
@@ -0,0 +1,12 @@
+package ArrayList;
+import java.util.ArrayList;
+
+public class intro {
+    public static void main(String[] args) {
+        ArrayList<Integer> intList = new ArrayList<>();
+        ArrayList<String> strList = new ArrayList<>();
+
+        intList.add(69);
+        intList.add(911);
+    }
+}
---
+++ BackTracking/SubSet.java
@@ -0,0 +1,9 @@
+public class SubSet {
+    
+    public void SubSet (String word, String ans, int index) {
+
+    }
+    public static void main(String[] args) {
+        String word = "ABC";
+    }
+}
